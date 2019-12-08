d8f7e523a0507c56ba62ce42f155d943eb543cbe
arunatennakoon <arunatennakoon1984@gmail.com>
2019-12-09 00:24:20 +1300

    Adding prop-types dependancy

--- src/components/Food/Food/FoodIngredients/FoodIngredients.js
+++ src/components/Food/Food/FoodIngredients/FoodIngredients.js
@@ -1,43 +1,48 @@
-import React from 'react'; 
+import React from 'react';
 import classes from './FoodIngredients.css'
+import PropTypes from 'prop-types';
 
 const FoodIngredients = (props) => {
 
     let ingredients = null;
 
-    switch(props.type){
-        case('bread-bottom'):
+    switch (props.type) {
+        case ('bread-bottom'):
             ingredients = <div className={classes.BreadBottom}></div>;
             break;
-            case('bread-top'):
+        case ('bread-top'):
             ingredients = <div className={classes.BreadTop}>
                 <div className={classes.Seeds1}></div>
                 <div className={classes.seeds2}></div>
             </div>;
             break;
-            case('seeds1'):
+        case ('seeds1'):
             ingredients = <div className={classes.Seeds1}></div>;
             break;
-            case('seeds2'):
+        case ('seeds2'):
             ingredients = <div className={classes.Seeds2}></div>;
             break;
-            case('meat'):
+        case ('meat'):
             ingredients = <div className={classes.Meat}></div>;
             break;
-            case('cheese'):
+        case ('cheese'):
             ingredients = <div className={classes.Cheese}></div>;
             break;
-            case('salad'):
+        case ('salad'):
             ingredients = <div className={classes.Salad}></div>;
             break;
-            case('bacon'):
+        case ('bacon'):
             ingredients = <div className={classes.Bacon}></div>;
             break;
-            case('style'):
+        case ('style'):
             ingredients = <div className={classes.style}></div>;
             break;
+        case ('default'):
+        default:
+            ingredients = null;
     }
-
+    return ingredients;
 }
+
 
 export default FoodIngredients;
